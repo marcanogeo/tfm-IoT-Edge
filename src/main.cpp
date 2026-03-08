4d2a97d7d83fc453fa5a81b5fd7a46fcc7bd6f15
georgelysmarcano <geogemar@hotmail.com>
2026-03-08 20:16:42 +0100

    update to create instances from node_id

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <PubSubClient.h>
 
 #define MQ2pin 14 //Pin where the MQ2 sensor is connected
+const int DHT_PIN = 27; // Pin where the DHT22 sensor is connected
 
 //Setting WiFi credentials
 const char* SSID = "Wokwi-GUEST";
@@ -18,12 +19,36 @@ const char* psswd = "";
 const char* mqtt_server = "broker.hivemq.com";
 const int mqtt_port = 1883;
 
-const char* NODE_ID = "nodo1";
-const char* ZONE_ID = "zona_norte";
+#ifndef NODE_NUM
+#define NODE_NUM 1
+#endif
 
-const char* mqtt_topic_data = "tfm/ambiental/nodo1/telemetria";
-const char* mqtt_topic_status = "tfm/ambiental/nodo1/estado";
-const char* mqtt_topic_cmd = "tfm/ambiental/nodo1/cmd";
+#if NODE_NUM == 1
+  const char* NODE_ID = "nodo1";
+  const char* ZONE_ID = "zona_norte";
+#elif NODE_NUM == 2
+  const char* NODE_ID = "nodo2";
+  const char* ZONE_ID = "zona_sur";
+#elif NODE_NUM == 3
+  const char* NODE_ID = "nodo3";
+  const char* ZONE_ID = "zona_este";
+#elif NODE_NUM == 4
+  const char* NODE_ID = "nodo4";
+  const char* ZONE_ID = "zona_oeste";
+#else
+  const char* NODE_ID = nodoX;
+  const char* ZONE_ID = zonaX;
+#endif      
+//--------Nodo1 -----------//
+//const char* NODE_ID = "nodo1";
+//const char* ZONE_ID = "zona_norte";
+//const char* mqtt_topic_data = "tfm/ambiental/nodo1/telemetria";
+//const char* mqtt_topic_status = "tfm/ambiental/nodo1/estado";
+//const char* mqtt_topic_cmd = "tfm/ambiental/nodo1/cmd";
+
+String mqtt_topic_data;
+String mqtt_topic_status;
+String mqtt_topic_cmd;
 
 //Conexión a thingsboard.io
 //String tbhost = "https://demo.thingsboard.io";
@@ -38,8 +63,6 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 DHTesp dhtSensor; // Create DHT sensor object
 
-const int DHT_PIN = 27; // Pin where the DHT22 sensor is connected
-
 unsigned long lastPublish = 0;
 const unsigned long publishInterval = 5000; // Publish every 5 seconds
 
@@ -48,6 +71,8 @@ int gasValue = 0; // Variable to store the gas sensor value
 
  // WiFi connection function
 void connectWiFi(){
+
+  if(WiFi.status() == WL_CONNECTED) return;
 
   WiFi.mode(WIFI_STA);
   //WiFi.begin(SSID.c_str(), psswd.c_str());
@@ -65,7 +90,7 @@ void connectWiFi(){
   if (WiFi.status() == WL_CONNECTED) {
     delay(1000);
     Serial.println("\nConnected to WiFi");
-    Serial.println("IP address: ");
+    //Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
   } else {
     Serial.println("\nWiFi connection failed");
@@ -109,13 +134,13 @@ void reconnectMQTT() {
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       //client.subscribe("sensor/wokwi/#");
-      Serial.println("Client ID: " + clientId);
-      Serial.println("Topic Telemetria: " + String(mqtt_topic_data));
-      Serial.println("Topic status: " + String(mqtt_topic_status));
+      //Serial.println("Client ID: " + clientId);
+      //Serial.println("Topic Telemetria: " + String(mqtt_topic_data));
+      //Serial.println("Topic status: " + String(mqtt_topic_status));
     
-      client.subscribe(mqtt_topic_cmd);
+      client.subscribe(mqtt_topic_cmd.c_str());
 
-      bool ok1 = client.publish(mqtt_topic_status, "online", true);
+      bool ok1 = client.publish(mqtt_topic_status.c_str(), "online", true);
      // bool ok2 = client.publish("tfm/debug/nodo1", "hola_desde_wokwi", true);
 
       Serial.println(ok1 ? "Estado retained ok": "Estado retained ERROR");
@@ -126,8 +151,8 @@ void reconnectMQTT() {
       Serial.print("failed, rc=");
       Serial.println(client.state());
       Serial.println(" try again in 5 seconds");
-      //delay(5000);
-      millis() > 5000 ? delay(0) : delay(5000); // Non-blocking delay
+      delay(5000);
+      //millis() > 5000 ? delay(0) : delay(5000); // Non-blocking delay
     }
   }
 }
@@ -192,12 +217,12 @@ void publishTelemetry(){
   //client.publish(mqtt_topic_gas, String(sensorValue).c_str());
   //client.publish(mqtt_topic_payload, payload.c_str());
   //client.publish("sensor/wokwi/payload", payload.c_str());
-  bool ok = client.publish(mqtt_topic_data, payload.c_str());
+  bool ok = client.publish(mqtt_topic_data.c_str(), payload.c_str());
 
   Serial.println("--------Telemetria MQTT --------");
-  Serial.println("Temperature:" + String(data.temperature, 2) + "ºC");
-  Serial.println("Humidity: " + String(data.humidity, 1) + " %");
-  Serial.println(String("Gas: ") + (gasValue == 0 ? "SI" : "NO"));
+  //Serial.println("Temperature:" + String(data.temperature, 2) + "ºC");
+  //Serial.println("Humidity: " + String(data.humidity, 1) + " %");
+  //Serial.println(String("Gas: ") + (gasValue == 0 ? "SI" : "NO"));
   Serial.println("Payload: " + payload);
   Serial.println(ok ? "Publicación MQTT correcta" : "Error al publicar MQTT");
   Serial.println("------------------------");
@@ -211,6 +236,13 @@ void setup(){
 
   randomSeed(esp_random()); // Initialize random seed for MQTT client ID
 
+  mqtt_topic_data = "tfm/ambiental/" + String(NODE_ID) + "/telemetria";
+  mqtt_topic_status = "tfm/ambiental/" + String(NODE_ID) + "/estado";
+  mqtt_topic_cmd = "tfm/ambiental/" + String(NODE_ID) + "/cmd";
+
+  Serial.println("Nodo configurado: " + String(NODE_ID));  
+  Serial.println("Zona configurada: " + String(ZONE_ID));
+  
   connectWiFi();
 
   dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
@@ -234,7 +266,7 @@ void loop(){
    if(!client.connected()){
     reconnectMQTT();
   }
-  
+
   client.loop();
 
   unsigned long now = millis();
